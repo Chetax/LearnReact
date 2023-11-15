58de41e7a40b697ff3654f4fa97eb6e346e25a01
Chetan Navnath Padhen <padhenchetan@gmail.com>
2023-11-15 22:49:08 +0530

    Cahges Add

--- Project/JSX/src/Compnent/Home.jsx
+++ Project/JSX/src/Compnent/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import './Home.css';
+function ChangeTheam({color}){
+console.log("Change Color To ",String(color));
+}
 const Home=()=>{
-    return<>
+    return <>
     <div className="Headline">    
        <h1 style={{fontSize:"70px"}}>Hello <span style={{color:"aqua"}}>And</span>  Welcome</h1>
        <h3 style={{fontSize:"40px",textDecoration:"underline"}}>Let's Apply <span style={{color:"aqua"}}>JSX</span> 
@@ -11,6 +14,14 @@ const Home=()=>{
 
 <h3 style={{marginLeft:"50px",marginTop:"50px" ,fontSize:"25px"}}>Applying <span style={{color:"aqua"}}>If</span> Else Condtion : </h3>
 
+<div className="IfElseDiv">
+    <h3 style={{textAlign:"center",fontSize:'30px'}}>Chose One Option : </h3>
+    <div className="choise" style={{textAlign:"center",cursor:"pointer"}}>
+    <span className="Dark" style={{textAlign:"center",fontSize:'30px'}} onClick={() => console.log("dark")}>Dark </span> 
+    <span style={{textAlign:"center",fontSize:'30px'}}> Or </span> 
+    <span className="Aqua" style={{textAlign:"center",fontSize:'30px'}}  onClick={() => console.log("blue")}> Blue </span> 
+    </div>
+</div>
     </>
 }
 export default Home;
